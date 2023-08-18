fa8c827b3d1479c71358f92620a60f3f0fb1e718
lucasedferreira <lucasedbatista@gmail.com>
2023-08-18 16:44:35 -0300

    setup docker coposer with postgres, php and adminer

---
+++ db/create-db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE product_categories (
+  id SERIAL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  price decimal NOT NULL,
+  tax decimal NOT NULL,
+  category_id integer NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now(),
+  FOREIGN KEY (category_id)
+    REFERENCES product_categories (id)
+);
